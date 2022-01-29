81c6b97be9f71436bc252ac1641fab4f59abdbb5
MaiAnh871 <94096485+MaiAnh871@users.noreply.github.com>
2022-01-29 12:48:45 +0700

    Update text1.m

--- ex2/text1.m
+++ ex2/text1.m
@@ -5,7 +5,7 @@ X = data (:, [1, 2]);
 y = data (:, 3);
 
 %% Plot the data with + indicating (y = 1) examples and o indicating (y = 0) examples.
-%plotData(X, y);
+plotData(X, y);
  
 % Labels and Legend
 xlabel('Exam 1 score')
@@ -16,3 +16,25 @@ legend('Admitted', 'Not admitted')
 
 %% Provide input values to the sigmoid function below and run to check your implementation
 sigmoid(0);
+
+%% Cost function and gradient
+%  Setup the data matrix appropriately
+[m, n] = size(X);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
+% Initialize the fitting parameters
+initial_theta = zeros (n + 1, 1);
+
+% Compute and display the initial cost and gradient
+[cost, grad] = costFunction(initial_theta, X, y);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+disp('Gradient at initial theta (zeros):'); 
+disp(grad);
+
+% Compute and display cost and gradient with non-zero theta
+test_theta = [-24; 0.2; 0.2];
+[cost, grad] = costFunction(test_theta, X, y);
+fprintf('\nCost at non-zero test theta: %f\n', cost);
+disp('Gradient at non-zero theta:'); disp(grad);
