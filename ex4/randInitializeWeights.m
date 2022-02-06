47dedd584a52648e067f77e2f094bc20ba5bb70b
MaiAnh871 <94096485+MaiAnh871@users.noreply.github.com>
2022-02-06 08:43:36 +0700

    Create randInitializeWeights.m

---
+++ ex4/randInitializeWeights.m
@@ -0,0 +1,4 @@
+function [W] = randInitializeWeights(L_in, L_out)
+% Randomly initialize the weights to small values
+    epsilon_init = 0.12;
+    W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
