7fc9a2d1319c5a06c5e3240e925b631250ef1c98
MaiAnh871 <94096485+MaiAnh871@users.noreply.github.com>
2022-02-06 08:42:28 +0700

    Update implement.m

--- ex4/implement.m
+++ ex4/implement.m
@@ -55,3 +55,23 @@ checkNNGradients(lambda);
 debug_J  = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambda);
 fprintf('Cost at (fixed) debugging parameters (w/ lambda = 3): %f', debug_J);
 
+%% Learning parameters using fmincg
+options = optimset('MaxIter', 50);
+lambda = 1;
+
+% Create "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, num_labels, X, y, lambda);
+
+% Now, costFunction is a function that takes in only one argument (the
+% neural network parameters)
+[nn_params, ~] = fmincg(costFunction, initial_nn_params, options);
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), hidden_layer_size, (input_layer_size + 1));
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), num_labels, (hidden_layer_size + 1));
+ 
+pred = predict(Theta1, Theta2, X);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+%% Visualizing the hidden layer
+% Visualize Weights 
+displayData(Theta1(:, 2:end));
