e0285d02d604dfd916465b08e7984604e957c5e7
MaiAnh871 <94096485+MaiAnh871@users.noreply.github.com>
2022-02-10 07:51:55 +0700

    Update implement.m

--- ex5/implement.m
+++ ex5/implement.m
@@ -40,6 +40,7 @@ hold off;
 lambda = 0;
 [error_train, error_val] = learningCurve([ones(m, 1) X], y, [ones(size(Xval, 1), 1) Xval], yval, lambda);
 
+figure;
 plot(1:m, error_train, 1:m, error_val);
 title('Learning curve for linear regression')
 legend('Train', 'Cross Validation')
@@ -53,3 +54,93 @@ for i = 1:m
 end
 
 %% 3. Polynomial regression
+%% 3.1 Learning Polynomial Regression
+p = 8;
+
+% Map X onto Polynomial Features and Normalize
+X_poly = polyFeatures(X, p);
+[X_poly, mu, sigma] = featureNormalize(X_poly);  % Normalize
+X_poly = [ones(m, 1), X_poly];                   % Add Ones
+
+% Map X_poly_test and normalize (using mu and sigma)
+X_poly_test = polyFeatures(Xtest, p);
+X_poly_test = X_poly_test-mu; % uses implicit expansion instead of bsxfun
+X_poly_test = X_poly_test./sigma; % uses implicit expansion instead of bsxfun
+X_poly_test = [ones(size(X_poly_test, 1), 1), X_poly_test];         % Add Ones
+
+% Map X_poly_val and normalize (using mu and sigma)
+X_poly_val = polyFeatures(Xval, p);
+X_poly_val = X_poly_val-mu; % uses implicit expansion instead of bsxfun
+X_poly_val = X_poly_val./sigma; % uses implicit expansion instead of bsxfun
+X_poly_val = [ones(size(X_poly_val, 1), 1), X_poly_val];           % Add Ones
+
+fprintf('Normalized Training Example 1:\n');
+fprintf('  %f  \n', X_poly(1, :));
+
+% Train the model
+lambda = 0;
+[theta] = trainLinearReg(X_poly, y, lambda);
+
+% Plot training data and fit
+figure;
+plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+plotFit(min(X), max(X), mu, sigma, theta, p);
+xlabel('Change in water level (x)');
+ylabel('Water flowing out of the dam (y)');
+title (sprintf('Polynomial Regression Fit (lambda = %f)', lambda));
+
+figure;
+[error_train, error_val] = learningCurve(X_poly, y, X_poly_val, yval, lambda);
+plot(1:m, error_train, 1:m, error_val);
+title(sprintf('Polynomial Regression Learning Curve (lambda = %f)', lambda));
+xlabel('Number of training examples')
+ylabel('Error')
+axis([0 13 0 100])
+legend('Train', 'Cross Validation')
+
+% 3.2 Optional (ungraded) exercise: Adjusting the regularization parameter
+% Choose the value of lambda
+lambda = 1;
+[theta] = trainLinearReg(X_poly, y, lambda);
+
+% Plot training data and fit
+figure;
+plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+plotFit(min(X), max(X), mu, sigma, theta, p);
+xlabel('Change in water level (x)');
+ylabel('Water flowing out of the dam (y)');
+title (sprintf('Polynomial Regression Fit (lambda = %f)', lambda));
+figure;
+[error_train, error_val] = learningCurve(X_poly, y, X_poly_val, yval, lambda);
+plot(1:m, error_train, 1:m, error_val);
+title(sprintf('Polynomial Regression Learning Curve (lambda = %f)', lambda));
+xlabel('Number of training examples')
+ylabel('Error')
+axis([0 13 0 100])
+legend('Train', 'Cross Validation')
+
+% 3.3 Selecting lambda using a cross validation set
+[lambda_vec, error_train, error_val] = validationCurve(X_poly, y, X_poly_val, yval);
+figure;
+plot(lambda_vec, error_train, lambda_vec, error_val);
+legend('Train', 'Cross Validation');
+xlabel('lambda');
+ylabel('Error');
+for i = 1:length(lambda_vec)
+    if i == 1
+        fprintf('lambda\t\tTrain Error\tValidation Error\n');
+    end
+    fprintf('%f\t%f\t%f\n',lambda_vec(i), error_train(i), error_val(i));
+end
+
+% 3.5 Optional (ungraded) exercise: Plotting learning curves with randomly selected examples
+lambda = 0.01;
+[error_train, error_val] = learningCurve(X_poly, y, X_poly_val, yval, lambda);
+figure;
+plot(1:m, error_train, 1:m, error_val);
+
+title(sprintf('Polynomial Regression Learning Curve (lambda = %f)', lambda));
+xlabel('Number of training examples')
+ylabel('Error')
+axis([0 13 0 100])
+legend('Train', 'Cross Validation')
