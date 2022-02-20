ba4b2712e5b2ca4a84f5c3865401d943a35e4d33
MaiAnh871 <94096485+MaiAnh871@users.noreply.github.com>
2022-02-20 08:02:08 +0700

    Update implement.m

--- ex7/implement.m
+++ ex7/implement.m
@@ -9,3 +9,8 @@ initial_centroids = [3 3; 6 2; 8 5];
 % Find the closest centroids for the examples using the initial_centroids
 idx = findClosestCentroids(X, initial_centroids);
 fprintf('Closest centroids for the first 3 examples: %d %d %d', idx(1:3))
+
+%% 1.1.2 Computing centroid means
+% Compute means based on the closest centroids found in the previous part.
+centroids = computeCentroids(X, idx, K);
+fprintf('Centroids computed after initial finding of closest centroids: \n %f %f \n %f %f\n %f %f' , centroids');
