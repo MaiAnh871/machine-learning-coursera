c62a7fe206609fe37df0784fd57482fdab6c52d7
MaiAnh871 <94096485+MaiAnh871@users.noreply.github.com>
2022-02-21 07:30:09 +0700

    Update implement3.m

--- ex7/implement3.m
+++ ex7/implement3.m
@@ -10,3 +10,16 @@ plot(X(:, 1), X(:, 2), 'bo');
 axis([0.5 6.5 2 8]); axis square;
 
 %% 2.2 Implementing PCA
+% Before running PCA, it is important to first normalize X
+[X_norm, mu, ~] = featureNormalize(X);
+
+% Run PCA
+[U, S] = pca(X_norm);
+
+% Draw the eigenvectors centered at mean of data. These lines show the directions of maximum variations in the dataset.
+hold on;
+drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
+drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
+hold off;
+
+fprintf('Top eigenvector U(:,1) = %f %f \n', U(1,1), U(2,1));
